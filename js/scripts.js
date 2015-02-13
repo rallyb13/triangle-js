5a7616aedabaed0abb13d696b34a301561e20342
Benjamin Herson <benjaminherson@gmail.com>
2015-02-13 11:30:51 -0800

    add most of website, another function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var Triangle = function(a, b, c) {
+var triangle = function(a, b, c) {
 
   var result;
 
@@ -6,16 +6,48 @@ var Triangle = function(a, b, c) {
     result = "no actual"
   } else if (a === b && b === c) {
     result = "an equilateral";
-  } else if ((a === b && b !==c) | (a === c && b !== c) | (b === c && a !== b)){
+  } else if ((a === b && b !==c) | (a === c && b !== c) | (b === c && a !== b)) {
     result = "an isosceles";
   } else if (a !== b && a !== c && b !== c) {
     result = "a scalene";
   }
   return result;
+};
 
+var angleCheck = function(a,b,c) {
+  var result_two;
+  var smallerSides = Math.pow(a, 2) + Math.pow(b, 2);
+  var hypotenuse = Math.pow(c, 2);
+
+  if (triangle(a,b,c) === "no actual" | c < a | c < b) {
+    result_two = " ";
+  } else if (smallerSides === hypotenuse) {
+    result_two = "Right angle!";
+  } else if (smallerSides > hypotenuse) {
+    result_two = "What a-cutey!";
+  } else if (smallerSides < hypotenuse) {
+    result_two = "How obtuse!";
+  }
+  return result_two;
 };
 
 
-// Your sides: a b & c
-// Those create [] triangle
-// is it right? it ain't right. it's so right!
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+
+    var a = parseInt($("input#sideA").val());
+    var b = parseInt($("input#sideB").val());
+    var c = parseInt($("input#sideC").val());
+    var result1 = triangle(a, b, c);
+    var result2 = angleCheck(a, b, c);
+
+    $(".sideA").text(sideA);
+    $(".sideB").text(sideb);
+    $(".sideC").text(sidec);
+    $(".result1").text(result1);
+    $(".result2").text(result2);
+
+    $("#results").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,29 +1,51 @@
-describe('Triangle', function() {
+describe('triangle', function() {
   it("will recognize an equilateral triangle", function() {
-    expect(Triangle(3,3,3)).to.equal("an equilateral");
+    expect(triangle(3,3,3)).to.equal("an equilateral");
   });
 
   it("will recognize an isosceles triangle", function() {
-    expect(Triangle(2,1,2)).to.equal("an isosceles");
+    expect(triangle(2,1,2)).to.equal("an isosceles");
   });
 
   it("will recognize a different isosceles, with c as different", function() {
-    expect(Triangle(3,3,4)).to.equal("an isosceles");
+    expect(triangle(3,3,4)).to.equal("an isosceles");
   });
 
   it("will recognize any isosceles", function() {
-    expect(Triangle(1,1,1.4)).to.equal("an isosceles");
+    expect(triangle(1,1,1.4)).to.equal("an isosceles");
   });
 
   it("will recognize a scalene triangle", function() {
-    expect(Triangle(3,4,5)).to.equal("a scalene");
+    expect(triangle(3,4,5)).to.equal("a scalene");
   });
 
   it("will recognize when three sides cannot make a triangle", function() {
-    expect(Triangle(1,1,7)).to.equal("no actual");
+    expect(triangle(1,1,7)).to.equal("no actual");
   });
 
   it("will recognize non-triangles even if the user fails to enter the longest side last (as instructed)", function() {
-    expect(Triangle(10, 1.5, 4)).to.equal("no actual");
+    expect(triangle(10, 1.5, 4)).to.equal("no actual");
+  });
+});
+
+describe('angleCheck', function() {
+  it("will return nothing if the longest side is not entered last (as instructed)", function() {
+    expect(angleCheck(4, 1, 1)).to.equal(" ");
+  });
+
+  it("will recognize a right triangle", function() {
+    expect(angleCheck(3,4,5)).to.equal("Right angle!");
+  });
+
+  it("will recognize an acute triangle", function() {
+    expect(angleCheck(2,2,2)).to.equal("What a-cutey!");
+  });
+
+  it("will recognize a triangle that has an obtuse angle", function() {
+    expect(angleCheck(3,3,5)).to.equal("How obtuse!");
+  });
+
+  it("will use the triangle function to give no answer for sides that aren't triangles", function() {
+    expect(angleCheck(1,1,7)).to.equal(" ")
   });
 });
